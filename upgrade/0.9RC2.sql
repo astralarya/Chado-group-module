b018f2e72a223bb046875421a2b4d3abea287a6d
Mara Kim <hacker.root@gmail.com>
2015-02-09 16:35:57 -0600

    Add unique

--- upgrade/0.9RC2.sql
+++ upgrade/0.9RC2.sql
@@ -10,25 +10,23 @@ CREATE TABLE grp_contact (
 
 --Alter grpmember to track table type
 ALTER TABLE grpmember
-  ADD COLUMN linking_table information_schema.sql_identifier NOT NULL;
+  ADD COLUMN linking_table information_schema.sql_identifier NOT NULL,
+  ADD UNIQUE(grpmember_id, linking_table);
+
 
 --Alter xxx_grpmember tables to enforce single member design
 ALTER TABLE organism_grpmember
   DROP CONSTRAINT "organism_grpmember_grpmember_id_organism_id_key",
   ADD COLUMN linking_table information_schema.sql_identifier
     DEFAULT 'organism' CHECK (linking_table = 'organism'),
+  ADD FOREIGN KEY(grpmember_id, linking_table)
+    REFERENCES grpmember(grpmember_id, linking_table),
   ADD UNIQUE(grpmember_id);
 
 ALTER TABLE feature_grpmember
   DROP CONSTRAINT "feature_grpmember_grpmember_id_feature_id_key",
   ADD COLUMN linking_table information_schema.sql_identifier
     DEFAULT 'feature' CHECK (linking_table = 'feature'),
+  ADD FOREIGN KEY(grpmember_id, linking_table)
+    REFERENCES grpmember(grpmember_id, linking_table),
   ADD UNIQUE(grpmember_id);
-
-ALTER TABLE organism_grpmember
-  ADD FOREIGN KEY(grpmember_id, linking_table)
-    REFERENCES grpmember(grpmember_id, linking_table);
-
-ALTER TABLE feature_grpmember
-  ADD FOREIGN KEY(grpmember_id, linking_table)
-    REFERENCES grpmember(grpmember_id, linking_table);
